569232159fa0c1f060cd85c4922d5157a7a276bf
Eugenia <eugenia-g@mail.ru>
2024-01-17 16:13:09 +0300

    Homework1

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -12,4 +12,5 @@ public class HelloWorldTest {
                 .andReturn();
         response.prettyPrint();
     }
+
 }
---
+++ src/test/java/Homework1.java
@@ -0,0 +1,6 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("Hello fom Eugenia Novikova! Have a good day");
+    }
+    }
+
